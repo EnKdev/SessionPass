56c62117101d1cba438cd1ac1ab0d1d51f24f1d3
EnKdev <sesoui72@googlemail.com>
2024-11-22 14:04:05 +0100

    Kotlin (JVM) Version for SessionPass

---
+++ EnKdev.SessionPass.Kotlin/src/main/kotlin/xyz/enkdev/sessionpass/HttpHelper.kt
@@ -0,0 +1,39 @@
+﻿package xyz.enkdev.sessionpass
+
+import java.net.URI
+import java.net.URLEncoder
+import javax.net.ssl.HttpsURLConnection
+
+/**
+ * Provides mechanisms to send POST requests with a payload to a server
+ */
+class HttpHelper {
+    companion object {
+        /**
+         * Sends an HTTP POST request with a given payload to a server.
+         * 
+         * @param uri The target URL where the POST request should be sent to.
+         * @param pairs The payload of the POST request.
+         * 
+         * @returns A byte array containing the response from the webserver.
+         */
+        fun httpPost(uri: String, pairs: Map<String, String>): ByteArray? {
+            val url = URI(uri);
+            val conn = url.toURL().openConnection() as HttpsURLConnection;
+            
+            conn.requestMethod = "POST";
+            conn.doOutput = true;
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+            
+            val data = pairs.map {
+                "${URLEncoder.encode(it.key, "UTF-8")}=${URLEncoder.encode(it.value, "UTF-8")}"
+            }.joinToString("&")
+            
+            conn.outputStream.use() { os ->
+                os.write(data.toByteArray())
+            }
+            
+            return conn.inputStream.readBytes()
+        }
+    }
+}
---
+++ EnKdev.SessionPass.Kotlin/src/test/kotlin/xyz/enkdev/sessionpass/test/TestProgram.kt
@@ -0,0 +1,61 @@
+﻿package xyz.enkdev.sessionpass.test
+
+import xyz.enkdev.sessionpass.OtsvCode
+
+fun main() {
+    var otsvCode: String? = ""
+    var isSame: Boolean
+    var oc = OtsvCode()
+
+    println("TEST1: Code Check - Small (codeFlag=0)")
+    otsvCode = oc.requestOtsvCode(0)
+    println("Received One-Time-Session-Verification Code: ${otsvCode}")
+    println("Checking for integrity...")
+    isSame = oc.compareOtsvCode(otsvCode, 0)
+    evalComparison(isSame)
+
+    println("-----------")
+
+    println("TEST2: Code Check - Middle (codeFlag=1)")
+    otsvCode = oc.requestOtsvCode(1)
+    println("Received One-Time-Session-Verification Code: ${otsvCode}")
+    println("Checking for integrity...")
+    isSame = oc.compareOtsvCode(otsvCode, 1)
+    evalComparison(isSame)
+
+    println("-----------")
+
+    println("TEST3: Code Check - Large (codeFlag=2)")
+    otsvCode = oc.requestOtsvCode(2)
+    println("Received One-Time-Session-Verification Code: ${otsvCode}")
+    println("Checking for integrity...")
+    isSame = oc.compareOtsvCode(otsvCode, 2)
+    evalComparison(isSame)
+
+    println("-----------")
+
+    println("TEST4: Code Faking")
+    println("Setting otsvCode to abskd")
+    otsvCode = "abskd"
+    println("Checking for integrity...")
+    isSame = oc.compareOtsvCode(otsvCode, 0)
+    evalComparison(isSame)
+
+    println("-----------")
+
+    println("TEST5: Null-Terminated string")
+    println("Setting otsvCode to null")
+    otsvCode = null
+    println("Checking for integrity...")
+    isSame = oc.compareOtsvCode(otsvCode, 0)
+
+    readln()
+}
+    
+private fun evalComparison(isSame: Boolean) {
+    if (isSame) {
+        println("Code integrity is OK.")
+    } else {
+        println("Code integrity is NOT OK.")
+    }
+}
